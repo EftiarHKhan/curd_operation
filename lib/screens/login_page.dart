579a7806671b18379874bae00bb993d80097b0a8
EftiarHKhan <mdeftiar.official@gmail.com>
2023-06-15 00:48:13 +0600

    home page

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,8 +1,10 @@
+import 'package:curd_operation/auth.dart';
 import 'package:curd_operation/screens/signup_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage2 extends StatelessWidget {
   const LoginPage2({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
